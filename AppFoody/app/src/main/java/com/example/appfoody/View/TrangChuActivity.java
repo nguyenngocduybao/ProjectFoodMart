52d4d402db4bcf1e6ba17dc3382229321c84496c
bachsoda326 <sadok125a@gmail.com>
2019-02-13 00:32:53 +0700

    Lam trang chu

--- AppFoody/app/src/main/java/com/example/appfoody/View/TrangChuActivity.java
+++ AppFoody/app/src/main/java/com/example/appfoody/View/TrangChuActivity.java
@@ -1,13 +1,15 @@
-package com.example.appfoody;
+package com.example.appfoody.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.appfoody.R;
+
+public class TrangChuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_trangchu);
     }
 }
